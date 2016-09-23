89c485203688411b2fcd4cf16f93d42e329fcfed
joncicb <joncicb@gmail.com>
2016-09-23 20:00:02 +0200

    09.23. work at home testovi search, categories etc.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -8,6 +8,40 @@ class IndexController extends Zend_Controller_Action
     }
 
     public function indexAction() {
+        
+        $request = $this->getRequest();
+        $flashMessenger = $this->getHelper('FlashMessenger');
+        $form = new Application_Form_Frontend_Contact();
+        $systemMessages = 'init';
+        if ($request->isPost() && $request->getPost('task') === 'contact') {
+            try {
+                //check form is valid
+                if (!$form->isValid($request->getPost())) {
+                    throw new Application_Model_Exception_InvalidInput('Invalid form data bla bla');
+                }
+                //get form data
+                $formData = $form->getValues();
+                // do actual task
+                //save to database etc
+                $mailHelper = new Application_Model_Library_MailHelper();
+                $from_email = $formData['email'];
+                $to_email = 'joncicb@gmail.com';
+                $from_name = $formData['name'];
+                
+                $result = $mailHelper->sendmail($to_email, $from_email, $from_name);
+                
+                if(!$result){
+                   $systemMessages = 'Error';
+                }else{
+                   $systemMessages = 'Success';  
+                }
+                
+                
+            } catch (Application_Model_Exception_InvalidInput $ex) {
+                
+            }
+        }
+        
         $cmsIndexSlidesDbTable = new Application_Model_DbTable_CmsIndexSlides();
 
         $indexSlides = $cmsIndexSlidesDbTable->search(array(
@@ -147,6 +181,8 @@ class IndexController extends Zend_Controller_Action
         $this->view->products = $products;
         $this->view->productsOnAction = $productsOnAction;
         $this->view->productsByDateAded = $productsByDateAded;
+        $this->view->systemMessages = $systemMessages;
+        $this->view->form = $form;
         
     }
 
